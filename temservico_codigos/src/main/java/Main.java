21af4e9f1153befc60288cc673d87b9105410606
lucasszalves <souzaalves.lucas.2017@gmail.com>
2026-06-17 12:16:01 -0300

    main file

---
+++ temservico_codigos/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import controller.MainController;
+
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        MainController main = new MainController();
+        main.inicia();
+    }
+}
